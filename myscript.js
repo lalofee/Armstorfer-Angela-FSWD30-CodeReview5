2838d0036db045ec2d546092350208d9c61232ce
Angela Armstorfer <armstorfer@silverserver.at>
2017-12-26 13:48:34 +0100

    corrected some errors

--- myscript.js
+++ myscript.js
@@ -1,68 +1,70 @@
-var profile1 =  {
+var persons = [
+ {
 			name: 'John',
 			surname: 'Doe',
 			favoritePerformers: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
 			age: 25,
    			myPhoto: "img/boy1.jpg",	
 			likes: 0    // numeric property used to store likes
-};
+},
                         
-var profile2 = {
+ {
 			name: 'Jane',
 			surname: 'Doe',
 			favoritePerformers: ["Billy Idol", "Metallica", "Nirvana"],
 			age: 25,
    			myPhoto: "img/girl2.jpg",	
 			likes: 0    // numeric property used to store likes
-};
+},
                         
-var profile3 = {
+ {
 			name: 'Maxina',
 			surname: 'Musterman',
 			favoritePerformers: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
 			age: 25,
    			myPhoto: "img/girl1.jpg",	
 			likes: 0    // numeric property used to store likes
-};
+},
                         
-var profile4 = {
+ {
 			name: 'Max',
 			surname: 'Musterman',
 			favoritePerformers: ["Justin Timberlake", "Ed Sheeran", "Emma Watson"],
 			age: 25,
    			myPhoto: "img/boy2.jpg",	
 			likes: 0    // numeric property used to store likes
-};
-        
+}
+];
+        console.log(persons[0].surname);
 			
-		document.getElementById("profile1_image").innerHTML = '<img src="' + profile1.myPhoto  + '">';
-		document.getElementById("profile1_name").innerHTML = "Name: " + profile1.name;
-		document.getElementById("profile1_surname").innerHTML = "Surname: " + profile1.surname;
-		document.getElementById("profile1_age").innerHTML = "Age: " + profile1.age;
+		document.getElementById("profile1_image").innerHTML = '<img src="' + persons[0].myPhoto  + '">';
+		document.getElementById("profile1_name").innerHTML = "Name: " + persons[0].name;
+		document.getElementById("profile1_surname").innerHTML = "Surname: " + persons[0].surname;
+		document.getElementById("profile1_age").innerHTML = "Age: " + persons[0].age;
 		
-		document.getElementById("profile2_image").innerHTML = '<img src="' + profile2.myPhoto  + '">';
-		document.getElementById("profile2_name").innerHTML = "Name: " + profile2.name;
-		document.getElementById("profile2_surname").innerHTML = "Surname: " + profile2.surname;
-		document.getElementById("profile2_age").innerHTML = "Age: " + profile2.age;
+		document.getElementById("profile2_image").innerHTML = '<img src="' + persons[1].myPhoto  + '">';
+		document.getElementById("profile2_name").innerHTML = "Name: " + persons[1].name;
+		document.getElementById("profile2_surname").innerHTML = "Surname: " + persons[1].surname;
+		document.getElementById("profile2_age").innerHTML = "Age: " + persons[1].age;
 		
 		
-		document.getElementById("profile3_image").innerHTML = '<img src="' + profile3.myPhoto  + '">';
-		document.getElementById("profile3_name").innerHTML = "Name: " + profile3.name;
-		document.getElementById("profile3_surname").innerHTML = "Surname: " + profile3.surname;
-		document.getElementById("profile3_age").innerHTML = "Age: " + profile3.age;
-		
-
-		document.getElementById("profile4_image").innerHTML = '<img src="' + profile4.myPhoto  + '">';
-		document.getElementById("profile4_name").innerHTML = "Name: " + profile4.name;
-		document.getElementById("profile4_surname").innerHTML = "Surname: " + profile4.surname;
-		document.getElementById("profile4_age").innerHTML = "Age: " + profile4.age;
+		document.getElementById("profile3_image").innerHTML = '<img src="' + persons[2].myPhoto  + '">';
+		document.getElementById("profile3_name").innerHTML = "Name: " + persons[2].name;
+		document.getElementById("profile3_surname").innerHTML = "Surname: " + persons[2].surname;
+		document.getElementById("profile3_age").innerHTML = "Age: " + persons[2].age;
 		
 
+		document.getElementById("profile4_image").innerHTML = '<img src="' + persons[3].myPhoto  + '">';
+		document.getElementById("profile4_name").innerHTML = "Name: " + persons[3].name;
+		document.getElementById("profile4_surname").innerHTML = "Surname: " + persons[3].surname;
+		document.getElementById("profile4_age").innerHTML = "Age: " + persons[3].age;
+		
 
 
 
 
-// like-counter, doesnt work properly
+
+// like-counter, doesnt work properly yet
 var clicks = 0;
     function likeCounter() {
 
@@ -71,63 +73,6 @@ var clicks = 0;
 
 };
     
-//--------solution with classes. but didnt find a solution for the image yet
-
-/*
-class Person {                  
-
-   
-    constructor(name, surname, age) {  
-        
-        this.name = name;        
-        this.surname = surname;
-        this.age = age;      
-    }
-
-    output() {
-    	
-		document.getElementById("profile1_name").innerHTML = "Name: " + this.name;
-		document.getElementById("profile1_surname").innerHTML = "Surname: " + this.surname;
-		document.getElementById("profile1_age").innerHTML = "Age: " + this.age;
-
-		document.getElementById("profile2_name").innerHTML = "Name: " + this.name;
-		document.getElementById("profile2_surname").innerHTML = "Surname: " + this.surname;
-		document.getElementById("profile2_age").innerHTML = "Age: " + this.age;
-		
-		document.getElementById("profile3_name").innerHTML = "Name: " + this.name;
-		document.getElementById("profile3_surname").innerHTML = "Surname: " + this.surname;
-		document.getElementById("profile3_age").innerHTML = "Age: " + this.age;
-		
-		document.getElementById("profile4_name").innerHTML = "Name: " + this.name;
-		document.getElementById("profile4_surname").innerHTML = "Surname: " + this.surname;
-		document.getElementById("profile4_age").innerHTML = "Age: " + this.age;
-		
-}
-
-}
-
-
-var profile1 = new Person("John", "Doe", "25");
-profile1.output();
-
-var profile2 = new Person("Jane", "Doe", "25");
-profile2.output();
-
-var profile3 = new Person("Maxina", "Musterman", "25");
-profile3.output();
-
-var profile4 = new Person("Max", "Musterman", "25");
-profile4.output();
-
-//	document.getElementById("profile2_image").innerHTML = '<img src="' + profile1.myPhoto  + '">';
-//	document.getElementById("profile2_image").innerHTML = '<img src="' + profile2.myPhoto  + '">';
-//	document.getElementById("profile3_image").innerHTML = '<img src="' + profile3.myPhoto  + '">';
-//	document.getElementById("profile4_image").innerHTML = '<img src="' + profile4.myPhoto  + '">';
-
-*/
-
-
-
 
 
 
